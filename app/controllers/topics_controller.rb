ce09e9b758854224bfe2a55d5e8c01ea53afc0c8
akht <picopeko@gmail.com>
2019-06-10 04:35:09 +0900

    各roomに紐づくtopicの保存

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,2 +1,13 @@
 class TopicsController < ApplicationController
+  def create
+    @room = Room.find(params[:room_id])
+    @room.topics.create(topic_params)
+    redirect_to room_path(@room)
+  end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:name)
+  end
 end
