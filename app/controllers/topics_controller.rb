49baedec99f970d3d6299b062610cd2192f68711
akht <picopeko@gmail.com>
2019-06-10 12:11:13 +0900

    トーク開始、話題を登録、全ての話題を表示のリンクをルーム詳細ページに追加

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,11 +1,28 @@
 class TopicsController < ApplicationController
+  before_action :set_room
+
+  def index
+    @topics = @room.topics
+  end
+
+  def new
+    @topics = @room.topics
+  end
+
+  def show
+    @topic = @room.topics.sample(1)
+  end
+
   def create
-    @room = Room.find(params[:room_id])
     @room.topics.create(topic_params)
     redirect_to room_path(@room)
   end
 
   private
+
+  def set_room
+    @room = Room.find(params[:room_id])
+  end
 
   def topic_params
     params.require(:topic).permit(:name)
