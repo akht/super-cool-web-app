26715b745f9db1ebdf5385e22c5c62f8baa8b77b
Masaki Komagata <komagata@gmail.com>
2020-05-21 13:53:09 +0900

    README変更

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_06_11_042123) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "rooms", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -21,10 +24,11 @@ ActiveRecord::Schema.define(version: 2019_06_11_042123) do
 
   create_table "topics", force: :cascade do |t|
     t.string "name"
-    t.integer "room_id"
+    t.bigint "room_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["room_id"], name: "index_topics_on_room_id"
   end
 
+  add_foreign_key "topics", "rooms"
 end
